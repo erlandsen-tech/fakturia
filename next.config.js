6a79cae79b35016adbf2b9607734d1ecf8209915
john inge <john@erlandsen.tech>
2025-06-09 21:21:06 +0200

    add amplify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // Remove static export since we have API routes
+  // output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   // config options here
 };
 
